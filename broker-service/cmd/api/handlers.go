08696ccd9643bdd7d510beacc32f8b398a472033
Muluneh Derese <mulunehd@ethiopianairlines.com>
2023-04-06 12:51:54 +0000

    feat: connect broker service to logger

--- broker-service/cmd/api/handlers.go
+++ broker-service/cmd/api/handlers.go
@@ -10,11 +10,17 @@ import (
 type RequestPaylod struct {
 	Action string     `json:"action"`
 	Auth   AuthPaylod `json:"auth,omitempty"`
+	Log    LogPayload `json:"log,omitempty"`
 }
 
 type AuthPaylod struct {
 	Email    string `json:"email"`
 	Password string `json:"password"`
+}
+
+type LogPayload struct {
+	Name string `json:"name"`
+	Data string `json:"data"`
 }
 
 func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
@@ -38,10 +44,46 @@ func (app *Config) HandleSubmission(w http.ResponseWriter, r *http.Request) {
 	switch requestPaylod.Action {
 	case "auth":
 		app.authenticate(w, requestPaylod.Auth)
+	case "log":
+		app.logItem(w, requestPaylod.Log)
 	default:
 		app.errorJSON(w, errors.New("Unknown action"))
 
 	}
+}
+
+func (app *Config) logItem(w http.ResponseWriter, entry LogPayload) {
+	jsonData, _ := json.MarshalIndent(entry, "", "\t")
+
+	logServiceURL := "http://logger-service/log"
+
+	request, err := http.NewRequest("POST", logServiceURL, bytes.NewBuffer(jsonData))
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+	request.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	response, err := client.Do(request)
+
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode != http.StatusAccepted {
+		app.errorJSON(w, err)
+		return
+	}
+
+	var payload jsonResponse
+	payload.Error = false
+	payload.Message = "logged"
+
+	app.writeJSON(w, http.StatusAccepted, payload)
 }
 
 func (app *Config) authenticate(w http.ResponseWriter, a AuthPaylod) {
